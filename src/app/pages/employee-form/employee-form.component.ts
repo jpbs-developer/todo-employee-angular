0727a49c08d94f4cf493ea0ad3eb5384217473bf
jpbs-developer <jpbs1@aluno.ifal.edu.br>
2023-03-18 13:28:06 -0300

    feat: build employee form

--- src/app/pages/employee-form/employee-form.component.ts
+++ src/app/pages/employee-form/employee-form.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-employee-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './employee-form.component.html',
   styleUrls: ['./employee-form.component.scss'],
 })
